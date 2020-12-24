5aad354df83f8cc9d1bd80373edf9c9be5d1cdc3
Alexander <alex@100up.ru>
2020-12-24 15:47:51 +0300

    init

---
+++ src/ModuleInstaller.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Jorique\BitrixModuleInstaller;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+/**
+ * Class ModuleInstaller
+ * @package Bitrix\Composer
+ */
+class ModuleInstaller extends LibraryInstaller
+{
+    const PACKAGE_TYPE = 'bitrix-module-installer';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPackageBasePath(PackageInterface $package)
+    {
+        $extras = $package->getExtra();
+
+        if ((array_key_exists('bitrix_module_name', $extras)) && (!empty($extras['bitrix_module_name']))) {
+            $name = (string)$extras['bitrix_module_name'];
+        } else {
+            throw new \Exception(
+                'Unable to install module, composer.json must contain module name declaration like this: ' .
+                '"extra": { "bitrix_module_name": "somename" } '
+            );
+        }
+
+        return 'bitrix/modules/'.$name;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return self::PACKAGE_TYPE === $packageType;
+    }
+}
---
+++ src/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Jorique\BitrixModuleInstaller;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+/**
+ * Class ModuleInstallerPlugin
+ * @package Osotov\BitrixPluginForComposer
+ */
+class Plugin implements PluginInterface
+{
+    /**
+     * @param Composer $composer
+     * @param IOInterface $io
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new ModuleInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
